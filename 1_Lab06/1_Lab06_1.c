76316f56edb1c50c4c70b6e1b9dca4c8f25eeb6a
y-eun <dmsdlsfla@gmail.com>
2023-05-31 21:20:16 +0900

    1_Lab06

--- 1_Lab06/1_Lab06_1.c
+++ 1_Lab06/1_Lab06_1.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 //1547
 int main() {
-	int n, i, a, b, ball;
-	ball = 1;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
-		scanf("%d %d", &a, &b);
-		if (a == ball)
-			ball = b;
-		else if (b == ball)
-			ball = a;
-	}
-	printf("%d", ball);
-	return 0;
+    int n, i, a, b, ball;
+    int* ballPtr;
+    ball = 1;
+    ballPtr = &ball; 
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+
+        if (a == *ballPtr)
+            *ballPtr = b;
+        else if (b == *ballPtr)
+            *ballPtr = a;
+    }
+
+    printf("%d", *ballPtr); 
+    return 0;
 }
